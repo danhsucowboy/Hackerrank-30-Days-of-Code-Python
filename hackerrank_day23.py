d419e54a9e23a40258774b96ef6d34f4ca25fda3
Dan Hsu <hsu86110@gmail.com>
2019-06-08 18:07:28 +0800

    Complete Day 23 BST Level-Order Traversal

---
+++ hackerrank_day23.py
@@ -0,0 +1,38 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        quete = [root]       
+        while quete:
+            node = quete.pop()
+            print(node.data, end = " ")
+            if node.left:
+                quete.insert(0,node.left)
+            if node.right:
+                quete.insert(0,node.right)
+        
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
